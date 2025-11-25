f90180deaf5c00fdf6ec395e5532a05af892c096
GhazMania <ghazwanrosyid.mr@gmail.com>
2025-11-25 15:07:34 +0700

    Weight

--- src/test.c
+++ src/test.c
@@ -12,7 +12,7 @@ int main() {
     bmi = weightKg / (heightMeters * heightMeters);
     
 	printf("Answer: %lf", bmi);
-    printf("Answer: %lf", bmi);git
+    printf("Answer: %lf", weightKg);
     
     return 0;
 }
